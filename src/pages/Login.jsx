ca2a4570a2735e6d8bc1ab995c58ad105d98427f
pranjal-sahu-developer <sahupranjal03@gmail.com>
2025-11-04 18:29:55 +0530

    change minor

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@
 //   const navigate = useNavigate();
 //    const [loading, setLoading] = useState(false);
 
-  
+
 //   const handleSubmit = async (e) => {
 //     e.preventDefault();
 //     setLoading(true);
@@ -123,7 +123,7 @@
 // export default Login
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Mail, Lock, LogIn ,Eye,EyeOff} from "lucide-react";
+import { Mail, Lock, LogIn, Eye, EyeOff } from "lucide-react";
 import { useAuth } from "../context/authContext";
 
 
@@ -236,22 +236,22 @@ const Login = () => {
                 className="w-full pl-10 pr-3 py-2.5 rounded-lg bg-white/10 text-white placeholder-gray-400 border border-gray-500/40 focus:border-orange-400 focus:ring-2 focus:ring-orange-400 outline-none transition"
                 required
               /> */}
-                <input
-      type={showPassword ? "text" : "password"}
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      placeholder="••••••••"
-      className="w-full pl-10 pr-10 py-2.5 rounded-lg bg-white/10 text-white placeholder-gray-400 border border-gray-500/40 focus:border-orange-400 focus:ring-2 focus:ring-orange-400 outline-none transition"
-      required
-    />
-    <button
-      type="button"
-      onClick={() => setShowPassword(!showPassword)}
-      className="absolute right-3 top-2.5 text-gray-400 hover:text-white transition"
-      aria-label="Toggle Password Visibility"
-    >
-      {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
-    </button>
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="••••••••"
+                className="w-full pl-10 pr-10 py-2.5 rounded-lg bg-white/10 text-white placeholder-gray-400 border border-gray-500/40 focus:border-orange-400 focus:ring-2 focus:ring-orange-400 outline-none transition"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 top-2.5 text-gray-400 hover:text-white transition"
+                aria-label="Toggle Password Visibility"
+              >
+                {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              </button>
             </div>
           </div>
 

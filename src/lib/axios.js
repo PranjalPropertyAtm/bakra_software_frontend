c9c292562d6101653ddab22bae80b36b4705dadf
PranjalPropertyAtm <pranjal.propertyatm@gmail.com>
2025-11-07 14:36:14 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://bakra-software-backend.onrender.com/api/",
- baseURL: "http://localhost:3000/api/",
+  baseURL: "https://bakra-software-backend.onrender.com/api/",
+ // baseURL: "http://localhost:3000/api/",
     withCredentials: true,
   headers: {
     "Content-Type": "application/json",

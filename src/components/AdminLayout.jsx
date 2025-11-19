72ed44d0a9e7c5f0a4c093073ae80946e19bc4a4
PranjalPropertyAtm <pranjal.propertyatm@gmail.com>
2025-11-19 11:06:15 +0530

    Update AdminLayout.jsx

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -12,12 +12,12 @@ const AdminLayout = () => {
   return (
     <div className="flex bg-gray-50 min-h-screen">
       <Sidebar />
-      <div className="flex-1 flex flex-col md:ml">
+      <div className="flex-1 flex flex-col md:ml-64 transition-all duration-300">
         <Navbar onLogout={logout} />
-      <main className="flex-1 p-6 md:ml">
-        <Outlet />
-      </main>
-    </div>
+        <main className="flex-1 p-6 transition-all duration-300">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };

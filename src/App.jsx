59b643f29fe3a9bdc64ac94dfd509850584638b2
PranjalPropertyAtm <pranjal.propertyatm@gmail.com>
2025-11-24 13:05:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,15 @@
-// import React from 'react'
-// import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-// import Login from './pages/Login.jsx';
-// import Dashboard from './pages/Dashboard.jsx';
-// import ProtectedRoutes from './utils/ProtectedRoutes.jsx';
-// import AdminLayout from './components/AdminLayout.jsx';
-// import Orders from './pages/Orders.jsx';
-// import Customers from './pages/Customers.jsx';
-// import Report from './pages/Reports.jsx';
-// import Associates from './pages/Associates.jsx';
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { SearchProvider } from './context/SearchContext.jsx';
-// import Settings from './pages/Settings.jsx';
-
-
-// function App() {
-//   return (
-//     <>
-//      <SearchProvider>
-//       <BrowserRouter>
-//         <Routes>
-//           <Route path='/login' element={<Login />} />
-
-//           <Route path='/' element={<ProtectedRoutes><AdminLayout /></ProtectedRoutes>} >
-//             <Route index element={<Navigate to="/login" />} replace />
-//             <Route path="/dashboard" element={<Dashboard />} />
-//             <Route path="/orders" element={<Orders />} />
-//             <Route path="/customers" element={<Customers />} />
-//              <Route path="/reports" element={<Report />} />
-//             <Route path="/associates" element={<Associates />} />
-//             <Route path="/settings" element={<Settings />} />
-//           </Route>
-
-//           {/* Catch-all: redirect unknown routes to login */}
-//           {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
-//         </Routes>
-//       </BrowserRouter>
-
-//       <ToastContainer
-//         position="top-right"
-//         autoClose={2500}
-//         hideProgressBar
-//         closeOnClick
-//         pauseOnHover
-//         draggable
-//         theme="light"
-//         style={{
-//           fontSize: "14px",
-//           fontWeight: 500,
-//         }}
-//       />
-//     </SearchProvider>
-//     </>
-//   )
-// }
-
-// export default App
-
 import React from 'react'
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Login from './pages/Login.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import ProtectedRoutes from './utils/ProtectedRoutes.jsx';
 import AdminLayout from './components/AdminLayout.jsx';
-import Orders from './pages/Orders.jsx';
+import Orders from './pages/Orders/Orders.jsx';
+import AddOrder from './pages/Orders/AddOrder.jsx';
 import Customers from './pages/Customers.jsx';
 import Report from './pages/Reports.jsx';
-import Associates from './pages/Associates.jsx';
+import Associates from './pages/Associates/Associates.jsx';
+import AddAssociate from './pages/Associates/addAssociate.jsx';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SearchProvider } from './context/SearchContext.jsx';
@@ -88,9 +31,12 @@ function App() {
           <Route element={<ProtectedRoutes><AdminLayout /></ProtectedRoutes>}>
             <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/orders' element={<Orders />} />
+            <Route path='/orders/add' element={<AddOrder />} />
+            <Route path="/orders/edit/:id" element={<AddOrder />} />
             <Route path='/customers' element={<Customers />} />
             <Route path='/reports' element={<Report />} />
             <Route path='/associates' element={<Associates />} />
+            <Route path='/associates/add' element={<AddAssociate />} />
             <Route path='/settings' element={<Settings />} />
           </Route>
 
@@ -118,4 +64,3 @@ function App() {
 }
 
 export default App
-

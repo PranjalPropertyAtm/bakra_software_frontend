9a6c747c6d9a9c6f7f5136d7569d3aba7952c724
PranjalPropertyAtm <pranjal.propertyatm@gmail.com>
2026-01-03 12:50:30 +0530

    Update AdminLayout.jsx

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,25 +1,62 @@
-import React from "react";
+// import React from "react";
+// import Sidebar from "../components/AdminSidebar";
+// import Navbar from "../components/Navbar";
+// import { useAuth } from "../context/authContext";
+// import { Outlet, useNavigate} from "react-router-dom";
+
+
+// const AdminLayout = () => {
+//     const navigate = useNavigate();
+//       const { logout } = useAuth();
+
+//   return (
+//     <div className="flex bg-gray-50 min-h-screen">
+//       <Sidebar />
+//     <div className="flex-1 flex flex-col md:ml-64 min-w-0">
+//         <Navbar onLogout={logout} />
+//         <main className="flex-1 p-6 transition-all duration-300">
+//           <Outlet />
+//         </main>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AdminLayout;
+
+
+import React, { memo, useCallback } from "react";
 import Sidebar from "../components/AdminSidebar";
 import Navbar from "../components/Navbar";
 import { useAuth } from "../context/authContext";
-import { Outlet, useNavigate} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-
-const AdminLayout = () => {
-    const navigate = useNavigate();
-      const { logout } = useAuth();
+const AdminLayout = memo(() => {
+  const { logout } = useAuth();
+  
+  const handleLogout = useCallback(() => {
+    logout();
+  }, [logout]);
 
   return (
     <div className="flex bg-gray-50 min-h-screen">
       <Sidebar />
-    <div className="flex-1 flex flex-col md:ml-64 min-w-0">
-        <Navbar onLogout={logout} />
-        <main className="flex-1 p-6 transition-all duration-300">
+
+        <div className="flex-1 flex flex-col md:ml-64 min-w-0">
+
+        {/* FIXED HEIGHT NAVBAR */}
+        <Navbar onLogout={handleLogout} />
+
+        {/* CONTENT SCROLLS, NAVBAR DOES NOT */}
+        <main className="flex-1 p-6 overflow-auto h-[calc(100vh-70px)]">
           <Outlet />
         </main>
+
       </div>
     </div>
   );
-};
+});
+
+AdminLayout.displayName = 'AdminLayout';
 
 export default AdminLayout;
